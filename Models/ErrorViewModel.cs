54574f7933c30341456446350942eefb549be215
TOMAS POLONSKY <47066838@ort>
2022-06-30 15:09:18 -0300

    create .NET MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Primer_Proyecto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
